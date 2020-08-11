0201d9bd4060f121a08c01fe07e16223b8e80d18
paul <bitowaqr@gmail.com>
2020-08-11 11:25:20 +0200

    backup ex 1

---
+++ backup_exercise1.R
@@ -0,0 +1,87 @@
+# Exercise 1 
+
+## R as a fancy calculator
+
+## Use R to answer the following questions for you:  
+
+#### 1.1 what is 1+1
+
+
+### 1.2 What is 3 times 17 ?
+
+
+### 1.3 Create an object d equal to 10.
+
+
+### 1.4 Divide d by 5.
+
+
+### 1.5 Overwrite d, let it now be equal to 20, and ???print??? d, to see the results
+
+
+
+### 1.6 Overwrite d (which is now 20) with 4 times d, and print d top see the result
+
+
+# 1.7 Overwrite d (which is now 80), with d times d, and print the result
+
+
+### 1.8 Create a new object A and set it equal to 3
+
+
+### 1.9 Create another new object res, let it be the product of d and A, and output the result
+
+
+### 1.10 Finally, take the average of d, A and res
+
+
+
+## Evaluations
+
+### 1.11 Is 4 greater than 2?
+
+
+### 1.12 Is 5 less than 3?
+
+
+### 1.13 Is 6.2 equal to 12.4/2?
+
+
+### 1.14 Is 5 equal to or greater than 4?
+
+
+
+### 1.15
+# I Create an object foo and set it to be 8
+# II Create another object bar, and set it to be 7
+# III Is foo divided by bar greater than 1.14 ?
+
+
+
+
+
+
+
+### Is 4 greater than 2?
+
+### 
+
+### 
+
+### 
+
+### 
+
+### 
+
+
+### 
+
+### 
+
+
+### 
+
+### 
+
+### 
