9fc1b71de2010e21b63dc03e1d1244273ce90c0b
Sarah Bates <s.e.bates@sheffield.ac.uk>
2022-03-22 15:39:46 +0000

    Added a recap exercise (from january course) in case more practise is required

---
+++ Exercises/Ex_recap.R
@@ -0,0 +1,24 @@
+# ____RECAP EXERCISE -----
+
+# 1. check the working enviroment, clear it if neccessary
+
+
+# 2. load framingham data 
+
+
+# 3. What percentage of participants has a systolic BP over 140?
+# (hint: you can use nrow(...) on a data frame, or length(...) on a vector
+
+
+# 4. create a new column in dat which gives the age in months, instead of years
+
+
+# 5. what is the maximum value for the age in months?
+
+
+# 6. create a data frame which only contains 
+# male participants with a chronic heart disease (TenYearCHD) 
+
+
+# 7. create a scatter plot with age on the x, and BMI on the y axis
+
